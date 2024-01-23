6954ef6ae12ae30d0f8b55b77681e9f5e949979f
Daminithakur2 <139676425+Daminithakur2@users.noreply.github.com>
2024-01-22 23:18:29 -0800

    Add files via upload

---
+++ array.js
@@ -0,0 +1,68 @@
+/*let arr=[ 2,3,4,5,6,7,8];
+console.log(arr);
+arr[1]=9;
+console.log(arr);*/
+var a=[1,2,3,4,5,6,7,];
+a.push(3);
+console.log(a);
+a.pop(3);
+console.log(a);
+a.shift();
+console.log(a)
+a.unshift(9);
+console.log(a);
+var b=[2,3,5,6,7,8]
+console.log(b);
+b[2]=15;
+b[3]=16;
+console.log(b);
+b.pop(8);
+console.log(b);
+b.shift();
+console.log(b);
+var c=[1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18];
+console.log( "original array:"+c);
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+c.pop();
+console.log(c);
+c.shift()
+c.shift()
+c.shift()
+console.log("update array:"+c);
+var d=[1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18];
+console.log("original array:"+d);
+let newar =d.slice(3,7);
+console.log(newar);
+var e=[2,3,4,5,6,7,8]
+console.log(e);
+e[2]=14;
+e[3]=15;
+console.log(e)
+e.pop();
+e.pop();
+e.pop();
+e.push(16);
+e.push(7);
+e.push(8);
+console.log(e)
+var h=[2,3,4,5,6,7,8]
+console.log(h);
+let newhar=h.splice(2,2,14,15,16);
+console.log(h)
+var t=[2,3,4,5,6,7,8]
+console.log(t)
+let newtee=t.splice(1,2,11,12,13)
+console.log(t)
+var a=[1,2,3,3,2,1,1,2,3]
+console.log(a);
+ var a=new Set(a);
+ console.log(a);
+
+
---
+++ javascript.js
@@ -0,0 +1,87 @@
+/*let a=10;
+console.log(a);
+var b=20;
+console.log(b)
+
+console.log(a)
+console.log(a)*/
+/*function add (a,b){
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a%b)
+    console.log(a*b)
+
+}
+add(2,4)
+add(7,8)*/
+/*function math(a,b,d,g){
+let f=a+b;
+console.log(f);
+ let s=f/d;
+ console.log(s);
+ let third=s+f+g/50;
+ console.log(third);
+}
+
+math(2,3,4,5);*/
+/*function science(a,b,c){
+let f=a+b+c;
+console.log(f);
+let s=f+50+200+(50 /100)
+console.log(s)
+let third=f+s-50*(20/100)
+console.log(third)
+}
+science(2,3,4);*/
+/*function level(a,b){
+let c=a+b;
+console.log(c);
+let d=c+20+(20/100);
+console.log(d);
+let e=d+c-(20/100)+200;
+console.log(e);
+}
+level(3,4)*/
+/*function person(a,b,c,e,f){
+let d=a+b+c;
+console.log(d);
+let second=d+e+50+(d+e)+(20/100);
+console.log(second);
+let third=(second+d+f+
+    (second+d+f)+(50/100));
+console.log(third)
+}
+person(1,2,3,4,5)*/
+/*function percentage(sub1, sub2, sub3, sub4, sub5){
+let percentage = (sub1 + sub2 + sub3 + sub4 + sub5) / 5;
+switch (percentage > 0) {
+    case percentage >= 90:
+        console.log("grade=A");
+        break;
+    case percentage >= 80:
+        console.log("grade=B");
+        break;
+    case percentage >= 70:
+        console.log("grade=C");
+        break;
+    case percentage >= 60:
+        console.log("grade=D");
+        break;
+    case percentage >= 50:
+        console.log("grade=E");
+        break;
+    case percentage >= 40:
+        console.log("grade=F");
+        break;
+    default:
+        console.log("failed");
+        break;
+  } 
+}
+percentage(77, 70, 90, 78, 88);*/
+/*let arrayelement= prompt("ARRAY OF CHARACTER:")*/
+let arr=[ 2,3,4,5,6,7];
+console.log(arr);
+
+
+
